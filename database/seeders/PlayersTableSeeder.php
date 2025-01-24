c73a95732a6523268b74cfaa7f78c0253179abc8
karan <kn@int.iqsay.com>
2025-01-24 14:09:26 +0530

    fixed

---
+++ database/seeders/PlayersTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+// database/seeders/PlayersTableSeeder.php
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+
+class PlayersTableSeeder extends Seeder
+{
+    public function run()
+    {
+        // Path to the JSON file
+        $path = storage_path('app/players.json');
+
+        // Read and decode JSON data
+        $json = File::get($path);
+        $data = json_decode($json, true);
+
+        // Insert the data into the database
+        foreach ($data['data'] as $player) {
+            DB::table('players')->insert([
+                'id' => $player['id'],
+                'name' => $player['name'],
+                'country' => $player['country'],
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+
+        $this->command->info('Players data inserted successfully!');
+    }
+}
